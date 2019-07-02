8216eb6df879f023482b6c59a92be13390354fb5
Mykol Benning <41842486+Mykolb@users.noreply.github.com>
2019-07-02 14:03:23 -0400

    Revert "Preparing to merge branches"

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ const taskInput = document.querySelector('#task')
 
 //load all event listeners
 const loadEventListeners = () => {
-    //dom load vent
-    document.addEventListener('DOMContentLoaded', getTasks)
     //add task event
     form.addEventListener('submit', addTask)
     // remove task event
@@ -26,31 +24,7 @@ const loadEventListeners = () => {
     filter.addEventListener('keyup', filterTasks)
 }
 
-//get task from ls Working
-const getTasks = () => {
-    let tasks;
-    localStorage.getItem('tasks') === null ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'))  
-    
 
-    tasks.forEach(task => {
-        // create li element
-        const li = document.createElement('li')
-        //add class, need collection class item b/c of material ui
-        li.className = 'collection-item'
-        //create text node and append 
-        li.appendChild(document.createTextNode(task))
-        //create new link element
-        const link = document.createElement('a')
-        //add class to a tag
-        link.className = 'delete-item secondary-content'
-        //add icon html
-        link.innerHTML = '<i class="fa fa-remove"></i>'
-        //append the link to li
-        li.appendChild(link)
-        //append li to ul 
-        taskList.appendChild(li)
-    })
-}
 
 
 //add task
@@ -61,7 +35,7 @@ const addTask = (event) => {
         alert('Please add a task')
     }
   
-    // create li element
+// create li element
     const li = document.createElement('li')
     //add class, need collection class item b/c of material ui
     li.className = 'collection-item'
@@ -77,38 +51,9 @@ const addTask = (event) => {
     li.appendChild(link)
     //append li to ul 
     taskList.appendChild(li)
-
-    //store in local storage
-    storeInLocalStorage(taskInput.value)
     //clear input
     taskInput.value = ''
 }
-
-
-
-//store task WORKING!!
-const storeInLocalStorage = task => {
-   let tasks;
-    localStorage.getItem('tasks') === null ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks')); {   
-    }
-
-     tasks.push(task)
-    localStorage.setItem('tasks', JSON.stringify(tasks)) 
-
-//    if(localStorage.getItem('tasks') === null) {
-//        tasks = []
-//    } else {
-//     tasks = JSON.parse(localStorage.getItem('tasks'))
-//    }
-//     tasks.push(task)
-//     localStorage.setItem('tasks', JSON.stringify(tasks)) 
-
-    console.log(tasks)
-    // tasks.Array.isArry(tasks)//giving me the number 11 in console LOCAL STORAGE NEEDED TO BE CLEARED! 
-    }
-
-
-
 
 //remove task working!
 const removeTask = (event) => {
@@ -117,29 +62,10 @@ const removeTask = (event) => {
         console.log(event.target) 
         if(confirm('Confirm: Are you sure you want to delete this item?')) {
 
-            event.target.parentElement.parentElement.remove()
-            //remove from local storage
-            removeTaskFromLocalStorage(event.target.parentElement.parentElement)
         }
     }
+    event.target.parentElement.parentElement.remove()
 }
-
-//remove from ls
-const removeTaskFromLocalStorage = taskItem => {
-    let tasks;
-    localStorage.getItem('tasks') === null ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks')) 
-
-    tasks.forEach((task, index) =>  {
-        if(taskItem.textContent === task) {
-            tasks.splice(index, 1) 
-        } 
-    })
-    localStorage.setItem('tasks', JSON.stringify(tasks)) 
-}
-
-console.log(removeTaskFromLocalStorage)
-
-
 
 //clear fn
 const clearTasks = () => {
@@ -149,17 +75,10 @@ const clearTasks = () => {
     while(taskList.firstChild) {
         taskList.removeChild(taskList.firstChild)
     }
-    //clear from ls
-    clearTasksFromLocalStorage()
-}
-
-//clear tasks from ls
-const clearTasksFromLocalStorage = () => {
-    localStorage.clear()
 }
 
 //filter tasks fn
-const filterTasks = event => {
+const filterTasks = (event) => {
     const text = event.target.value.toLowerCase()
     console.log('keys pressed', text)
 
